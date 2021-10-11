af0d98070f55cc9b5cfa0bd7ada128e276800c44
wolf4fun <wilczek4fun@gmail.com>
2021-10-11 21:09:54 +0200

    changed to response.data

--- taskedo-front/src/App.jsx
+++ taskedo-front/src/App.jsx
@@ -6,17 +6,13 @@ const App = () => {
   const [fetched, setFetched] = useState([])
 
   const fetchResponse = () => {
-    // axios.get('127.0.0.1:5000/').then(response => {
-    //   console.log(response);
-    // })
-    // GET request for remote image in node.js
     axios({
       method: 'get',
       url: 'http://localhost:5000',
 
     })
       .then(response =>{
-          console.log(response);
+          console.log(response.data);
       })
       .catch(error=>{
         console.log(error);
