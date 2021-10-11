ec2500afee11f85f84b9ce0927c131973a68603a
wolf4fun <wilczek4fun@gmail.com>
2021-10-11 19:15:02 +0200

    Adding basic flask app

---
+++ Backend/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET'])
+def get_something():
+    return jsonify({"Hello": "World"})
+
+if __name__ == "__main__":
+    app.run()
